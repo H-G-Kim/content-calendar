31b3426fc04c440090273238f248bde51f664886
H-G-Kim <heegenek@gmail.com>
2023-09-29 19:50:47 +1000

    -new content ArrayList;
    time: 1:00:00

--- src/main/java/com/heekim/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/heekim/contentcalendar/repository/ContentCollectionRepository.java
@@ -2,5 +2,16 @@ package com.heekim.contentcalendar.repository;
 
 //versus-Later the data access repository name will be just ContentRepository
 
+import com.heekim.contentcalendar.model.Content;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
 public class ContentCollectionRepository {
+    private final List<Content> content = new ArrayList<Content>();
+
+
+
+
 }
