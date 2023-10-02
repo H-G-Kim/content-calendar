21129a565217b9789e3fd8c00efff689842050b6
H-G-Kim <heegenek@gmail.com>
2023-10-02 15:31:36 +1100

    -made the contentcontroller + dummy data using postconstruct
    time: 1:08:00

---
+++ src/main/java/com/heekim/contentcalendar/controller/ContentController.java
@@ -0,0 +1,49 @@
+package com.heekim.contentcalendar.controller;
+
+
+import com.heekim.contentcalendar.model.Content;
+import com.heekim.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/content")
+
+public class ContentController {
+
+    private final ContentCollectionRepository repository;
+
+
+    //step 1.old way-but this creates extra repository instance-there should already be 1 in application context container
+    /*private ContentController(){
+        this.repository = new ContentCollectionRepository();
+    }*/
+
+    //step2.One way of injecting the already instacen in container-inject by argument
+    /*private ContentController(ContentCollectionRepository contentCollectionRepository){
+        this.repository = contentCollectionRepository;
+    }*/
+
+    //step3.An easier way is to use final for line 13  and then intellij suggestion-"add parameter constructor"
+    @Autowired //this is not needed if you have just 1 public constructor as in this case-but lets leave it
+    //it means there in an injection(autowired) of bean
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+    //1:08.08
+    @GetMapping("") //this is a @RequestMapping under hood-a subset
+    public List<Content> findAll(){
+        return repository.findAll();
+    }
+
+
+
+
+
+
+
+}
--- src/main/java/com/heekim/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/heekim/contentcalendar/repository/ContentCollectionRepository.java
@@ -3,18 +3,39 @@ package com.heekim.contentcalendar.repository;
 
 
 import com.heekim.contentcalendar.model.Content;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 //versus-Later the data access repository name will be just ContentRepository
+//@repository == @Component-see docs
 @Repository
-public class ContentCollectionRepository {
+public class  ContentCollectionRepository {
     private final List<Content> content = new ArrayList<>();
 
     public ContentCollectionRepository(){
+
+    }
+
+    public List<Content> findAll(){
+        return content;
+    }
+
+    //1:01.01
+    // ?? what exactly is optional type-what is old alterntive
+    public Optional<Content> findById(Integer id){
+        return content.stream().filter(c -> c.equals(id)).findFirst();
+
+    }
+
+    //post-construct -after dependency injection to do any "initialisation"
+    //temp way to put some data into application
+    @PostConstruct
+    private void contentDataInit(){
 
     }
 
